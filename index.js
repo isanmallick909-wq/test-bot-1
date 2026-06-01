7a9ee0849c2946dca05ee79f6126044f1f0343a8
isanmallick909-wq <isanmallick909@gmail.com>
2026-06-02 02:17:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,6 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+console.log("HELLO FROM BOT");
+console.log("NODE IS WORKING");
 
-console.log('Bot starting...');
-console.log('TOKEN EXISTS:', !!process.env.TOKEN);
-
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ]
-});
-
-client.once('ready', () => {
-  console.log(`${client.user.tag} is online!`);
-});
-
-client.on('error', console.error);
-
-client.login(process.env.TOKEN)
-  .then(() => {
-    console.log('Login successful!');
-  })
-  .catch((err) => {
-    console.error('LOGIN ERROR:');
-    console.error(err);
-    process.exit(1);
-  });
+setInterval(() => {
+  console.log("Still running...");
+}, 30000);
