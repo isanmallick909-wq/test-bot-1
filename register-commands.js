ef39b6b3bd46903881277428102168a47c1d7b29
isanmallick909-wq <isanmallick909@gmail.com>
2026-06-04 21:55:08 +0530

    Update register-commands.js

--- register-commands.js
+++ register-commands.js
@@ -1,1 +1,30 @@
+const { REST, Routes } = require('discord.js');
 
+const commands = [
+    {
+        name: 'setup-ticket',
+        description: 'Create the ticket panel'
+    }
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+
+        console.log('Started refreshing application commands.');
+
+        await rest.put(
+            Routes.applicationGuildCommands(
+                '1511077438732369971',
+                '1282249688489136150'
+            ),
+            { body: commands }
+        );
+
+        console.log('Successfully reloaded application commands.');
+
+    } catch (error) {
+        console.error(error);
+    }
+})();
