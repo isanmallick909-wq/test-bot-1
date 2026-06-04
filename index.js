1d6d2b3e6aabdcbfe93838f9e1cae7f7c81fcc6a
isanmallick909-wq <isanmallick909@gmail.com>
2026-06-04 21:42:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const fs = require('fs');
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
 });
 
+client.commands = new Collection();
+
+const commandFiles = fs
+    .readdirSync('./commands')
+    .filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
+
 client.once('ready', () => {
-  console.log(`${client.user.tag} is online!`);
+    console.log(`${client.user.tag} is online!`);
+});
+
+client.on('interactionCreate', async interaction => {
+
+    if (interaction.isChatInputCommand()) {
+
+        const command = client.commands.get(interaction.commandName);
+
+        if (!command) return;
+
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+
+            await interaction.reply({
+                content: 'There was an error while executing this command.',
+                ephemeral: true
+            });
+        }
+    }
 });
 
 client.login(process.env.TOKEN)
-  .catch(err => {
-    console.error('LOGIN ERROR:', err);
-  });
+    .catch(err => {
+        console.error('LOGIN ERROR:', err);
+    });
